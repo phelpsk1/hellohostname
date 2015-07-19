8229e5c92fe8905dff116d0273ba8eb2f7fb0129
Kevin Phelps <phelpsk1@gmail.com>
2015-07-19 03:35:02 -0400

    Initial Commit

---
+++ hellohostname.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  name, err := os.Hostname()
+  if err != nil {
+    fmt.Fprintf(w, "Oops: %v\n", err)
+    return
+  }
+  fmt.Fprintf(w, "Hello! I am %s", name)
+}
+
+func main() {
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":80",nil)
+}
